fec87291666ea5bca611f9f243153bc6961de7ff
mohtashim21 <mohteshim3276@gmail.com>
2024-06-12 20:55:57 +0530

    App.jsx code and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import Register from "./pages/Register"
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Contacts from "./pages/Contacts";
 
 const App = () => {
   return (
-    <div>
-      <Register />
-    </div>
-  )
-}
+    <BrowserRouter>
+      <ToastContainer />
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="*" element={<Navigate to="/register" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
