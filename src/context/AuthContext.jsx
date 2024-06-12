75f0e53aed338ddeec93fd8f83eb5dddad5c07f3
mohtashim21 <mohteshim3276@gmail.com>
2024-06-12 20:50:55 +0530

    created a context for authentication in the client sde

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        await axios.get("http://localhost:8000/auth/user/check", {
+          withCredentials: true,
+        });
+        setIsAuthenticated(true);
+      } catch (error) {
+        setIsAuthenticated(false);
+      }
+    };
+    checkAuth();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
