c45b4382b143ebfbc7bee276cd46fdd7a6fbc295
mohtashim21 <mohteshim3276@gmail.com>
2024-06-12 20:16:00 +0530

    created login and register page for user login

---
+++ src/pages/Register.jsx
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { Icon } from "react-icons-kit";
+import { eyeOff } from "react-icons-kit/feather/eyeOff";
+import { eye } from "react-icons-kit/feather/eye";
+import { useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { toast } from "react-toastify";
+
+const Register = () => {
+  const API_URL = "http://localhost:8000/auth/user/";
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [type, setType] = useState("password");
+  const [icon, setIcon] = useState(eyeOff);
+  const navigate = useNavigate();
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(
+        `${API_URL}/register`,
+        { username, password },
+        { withCredentials: true }
+      );
+      toast.success("Registration successful");
+      navigate("/login");
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status === 400 &&
+        error.response.data === "User already has an account with this username"
+      ) {
+        toast.error("account already exist with this username");
+      } else {
+        toast.error("Registration failed");
+      }
+    }
+  };
+
+  const handleToggle = () => {
+    if (type === "password") {
+      setIcon(eye);
+      setType("text");
+    } else {
+      setIcon(eyeOff);
+      setType("password");
+    }
+  };
+
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Register
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form onSubmit={handleRegister} className="space-y-6" method="POST">
+          <div>
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Username
+            </label>
+            <div className="mt-2">
+              <input
+                id="username"
+                name="username"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div className="mt-2 relative">
+            <input
+              id="password"
+              name="password"
+              type={type}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+            <span
+              className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
+              onClick={handleToggle}
+            >
+              <Icon icon={icon} size={20} />
+            </span>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Register
+            </button>
+          </div>
+        </form>
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Already have an account?{" "}
+          <NavLink
+            to="/login"
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >
+            Login
+          </NavLink>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
